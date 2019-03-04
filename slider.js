4510f71b724578640c2ad4d955b6836974ad2e61
Thalles Nascimento Carvalho <thallescarvalho12@gmail.com>
2019-03-04 20:40:51 -0300

    Update slider.js
    Removed an unnecessary console.log and added some better description for the methods

--- slider.js
+++ slider.js
@@ -13,12 +13,14 @@ class Slider {
     const itemIndex = this.itemsArray.indexOf(item);
     
     // Executa a animação passando o card da ultima opção para a posição atual
+    // Executes the animation, changing the item marked as next to current
     this.itemsArray[itemIndex].classes = 'slider-item slider-item--moving-to-current';
     setTimeout(() => {
       this.itemsArray[itemIndex].classes = 'slider-item slider-item--current';
     }, 600);
 
     // Verifica se o próximo item do slider é o 1o item do array, e se for, anima o ultimo item do array
+    // Checks if the item passed in as a parameter is the first one in the items array, and if it is, it executes the animation for the last one in the array
     if (itemIndex !== 0) {
       this.itemsArray[itemIndex - 1].classes = 'slider-item slider-item--moving-to-default';
       setTimeout(() => {
@@ -32,12 +34,12 @@ class Slider {
     }
 
     // Verifica a existencia de um próximo item no array passado para o slider, caso não possua, volta ao primeiro item
+    // Checks if the array contains an item after the one passed in as a parameter, and if it doesn't, it goes back to the first item
     if (this.itemsArray[itemIndex + 1]) {
       this.itemsArray[itemIndex + 1].classes = 'slider-item slider-item--next';
     } else {
       this.itemsArray[0].classes = 'slider-item slider-item--next';
     }
-    console.log(this.itemsArray);
   }
 
 }
